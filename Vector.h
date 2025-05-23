fa7d44be85f458ade2b4f6a2eb9ed5b6c19cc235
VasareM <vasare.milkovaite@mif.stud.vu.lt>
2025-05-23 14:58:16 +0300

    prirašyta daug funkcijų (trūksta implementacijų), testavimas c++ versijai

--- Vector.h
+++ Vector.h
@@ -28,18 +28,113 @@ public:
 
     // Capacity
 // ...
-void shrink_to_fit();
-void reserve(size_t);
+    void shrink_to_fit();
+    void reserve(size_t);
 
 // Modifiers
-void push_back(double);
-void pop_back();
-void resize(size_t);
-void resize(size_t, double);
+    void push_back(double);
+    void pop_back();
+    void resize(size_t);
+    void resize(size_t, double);
 // ...
-void reserve( size_t new_cap );
+    void reserve( size_t new_cap );
+
+    // Member types
+    using value_type = double;
+    using allocator_type = std::allocator<value_type>;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = value_type*;
+    using const_iterator = const value_type*;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    // Member functions
+    // copy assignment operator
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            delete[] _data;
+            _size = other._size;
+            _capacity = other._capacity;
+            _data = new double[_capacity];
+            std::copy(other._data, other._data + _size, _data);
+        }
+        return *this;
+    }
+    void assign(std::size_t count, double value) {
+        delete[] _data;
+        _data = new double[count];
+        _size = _capacity = count;
+        std::fill(_data, _data + count, value);
+    }
+    allocator_type get_allocator() const {
+        return allocator_type();
+    }
+    iterator begin() { return _data; }
+    const_iterator begin() const { return _data; }
+    const_iterator cbegin() const { return _data; }
+
+    iterator end() { return _data + _size; }
+    const_iterator end() const { return _data + _size; }
+    const_iterator cend() const { return _data + _size; }
+
+    reverse_iterator rbegin() { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+    const_reverse_iterator crbegin() const { return const_reverse_iterator(end()); }
+
+    reverse_iterator rend() { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    const_reverse_iterator crend() const { return const_reverse_iterator(begin()); }
+
+    std::size_t max_size() const {
+        return std::numeric_limits<std::size_t>::max() / sizeof(double);
+    }
+    iterator insert(iterator pos, const double& value) {
+        std::size_t index = pos - _data;
+
+        if (_size == _capacity)
+            reserve(_capacity == 0 ? 1 : 2 * _capacity);
+
+        for (std::size_t i = _size; i > index; --i) {
+            _data[i] = _data[i - 1];
+        }
+
+        _data[index] = value;
+        ++_size;
+
+        return _data + index;
+    }
+    // nebus emplace
+    iterator erase(iterator pos) {
+        if (pos >= _data + _size)
+            return end();
+
+        std::size_t index = pos - _data;
+
+        for (std::size_t i = index; i < _size - 1; ++i) {
+            _data[i] = _data[i + 1];
+        }
+
+        --_size;
+        return _data + index;
+    }
+    void swap(Vector& other) noexcept {
+        std::swap(_data, other._data);
+        std::swap(_size, other._size);
+        std::swap(_capacity, other._capacity);
+    }
 
 
+
+    // non member functions
+    // non member swap
+    inline void swap(Vector& a, Vector& b) noexcept {
+        a.swap(b);
+    }
 
 private:
     double* _data;
@@ -203,3 +298,6 @@ void Vector::pop_back() {
     if (_size > 0)
     --_size;
 }
+
+
+///papildymai, member functions:
