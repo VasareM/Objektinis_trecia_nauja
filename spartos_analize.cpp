ffb8b4534a89b285fc3b90dcb2703976c64ca527
VasareM <vasare.milkovaite@mif.stud.vu.lt>
2025-05-29 21:51:52 +0300

    lyg veikiantis primas spartos testas su push_back, bet ne vidurkis imamas

--- spartos_analize.cpp
+++ spartos_analize.cpp
@@ -20,25 +20,25 @@ std::pair<double,int> test_push_back_su(unsigned int sz)
         v.push_back(i);
     }
     auto pabaiga=std::chrono::high_resolution_clock::now();
-    auto trukme=std::chrono::duration_cast<std::chrono::seconds>(pabaiga-pradzia);
+    auto trukme=std::chrono::duration<double>(pabaiga-pradzia).count();
     return {trukme, perskirstymai};
 }
 
 int main()
 {
-    double vector_laikas=0.0, mano_vector_laikas=0.0;
-    int test_kartai=3;
+    //double vector_laikas=0.0, mano_vector_laikas=0.0;
+    //int test_kartai=3;
     int failu_dydziai[5]={10000, 100000, 1000000, 10000000, 100000000};
-    std::cout << std::setw(12) << "Size" 
+    std::cout << std::setw(12) << "Dydis" 
               << std::setw(20) << "std::vector (s)" 
-              << std::setw(15) << "Reallocs" 
+              << std::setw(15) << "Perskirstymai" 
               << std::setw(20) << "Vector (s)" 
-              << std::setw(15) << "Reallocs" << "\n";
+              << std::setw(15) << "Perskirstymai" << "\n";
     
     for (auto sz : failu_dydziai)
     {
         auto [std_time, std_realloc] = test_push_back_su<std::vector<int>>(sz);
-        auto [vec_time, vec_realloc] = test_push_back_su<Vector>(sz);
+        auto [vec_time, vec_realloc] = test_push_back_su<Vector<int>>(sz);
 
         std::cout << std::setw(12) << sz 
                   << std::setw(20) << std::fixed << std::setprecision(5) << std_time 
